e824bab49d3ddbbc46eb84e8ec1bec1285343fe6
Christopher Hamer <christopher.hamer@bt.com>
2024-02-21 11:03:36 +0000

    refactor: file package correctly walks the directories now for files

--- ansible/file.go
+++ ansible/file.go
@@ -2,11 +2,13 @@ package ansible
 
 import (
 	"bytes"
+	"fmt"
 	"gopkg.in/yaml.v3"
 	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
+	"slices"
 	"strings"
 )
 
@@ -14,7 +16,18 @@ var (
 	vaultHeader = []byte("$ANSIBLE_VAULT;1.1;AES256")
 )
 
-func walk(root string, run func(path string, yml map[string]yaml.Node)) error {
+type YamlEditor interface {
+	run(path string, yml map[string]yaml.Node)
+}
+
+type Yaml struct{}
+
+func (y Yaml) run(path string, yml map[string]yaml.Node) {
+	fmt.Println(path)
+	fmt.Println(yaml.Marshal(yml))
+}
+
+func Walk(root string, y YamlEditor) error {
 	return filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
@@ -23,14 +36,18 @@ func walk(root string, run func(path string, yml map[string]yaml.Node)) error {
 			return filepath.SkipDir
 		}
 
-		yml, err := openAndParseFile(path)
-		if yml == nil {
+		if !slices.Contains([]string{".yaml", ".yml"}, filepath.Ext(d.Name())) {
 			return nil
 		}
+
+		yml, err := openAndParseFile(path)
 		if err != nil {
 			return err
 		}
-		run(path, yml)
+		if yml == nil {
+			return nil
+		}
+		y.run(path, yml)
 		return nil
 	})
 }
@@ -46,6 +63,9 @@ func openAndParseFile(path string) (map[string]yaml.Node, error) {
 
 func ParseFile(file io.Reader) (map[string]yaml.Node, error) {
 	content, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
 	if bytes.HasPrefix(content, vaultHeader) {
 		return nil, nil
 	}
---
+++ ansible/file_test.go
@@ -0,0 +1,102 @@
+package ansible
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
+	"os"
+	"testing"
+)
+
+type MockYamlEditor struct {
+	mock.Mock
+	executedFiles []string
+}
+
+func (ye *MockYamlEditor) run(path string, _ map[string]yaml.Node) {
+	ye.executedFiles = append(ye.executedFiles, path)
+}
+
+func TestFile(t *testing.T) {
+	t.Run("marshal file into yaml object successfully", func(t *testing.T) {
+		var buffer bytes.Buffer
+		buffer.WriteString("my_val: value")
+		content, err := ParseFile(&buffer)
+
+		expected := map[string]yaml.Node{"my_val": {
+			Kind:        0x8,
+			Style:       0x0,
+			Tag:         "!!str",
+			Value:       "value",
+			Anchor:      "",
+			Alias:       (*yaml.Node)(nil),
+			Content:     []*yaml.Node(nil),
+			HeadComment: "",
+			LineComment: "",
+			FootComment: "",
+			Line:        1,
+			Column:      9}}
+
+		require.NoError(t, err)
+		assert.Equal(t, expected, content)
+	})
+
+	t.Run("walk will execute a function on each parsed yaml file", func(t *testing.T) {
+		tmpDir := t.TempDir()
+		fileNames := []string{tmpDir + "/values.yml", tmpDir + "/values2.yml"}
+
+		err := os.WriteFile(fileNames[0], []byte("my_var: value"), 777)
+		require.NoError(t, err)
+
+		err = os.WriteFile(fileNames[1], []byte("my_var: value"), 777)
+		require.NoError(t, err)
+
+		mockYamlEditor := MockYamlEditor{}
+		err = Walk(tmpDir, &mockYamlEditor)
+		require.NoError(t, err)
+		assert.Equal(t, mockYamlEditor.executedFiles[0], tmpDir+"/values.yml")
+		assert.Equal(t, mockYamlEditor.executedFiles[1], tmpDir+"/values2.yml")
+
+	})
+
+	t.Run("walking the directory should ignore non yaml files", func(t *testing.T) {
+		tmpDir := t.TempDir()
+
+		err := os.WriteFile(tmpDir+"/values.txt", []byte("my_var: value"), 777)
+		require.NoError(t, err)
+
+		mockYamlEditor := MockYamlEditor{}
+		err = Walk(tmpDir, &mockYamlEditor)
+		require.NoError(t, err)
+		assert.Empty(t, mockYamlEditor.executedFiles)
+
+	})
+
+	t.Run("walking the directory should ignore hidden directories", func(t *testing.T) {
+		tmpDir, err := os.MkdirTemp("", ".DS_STORE")
+		require.NoError(t, err)
+		defer os.RemoveAll(tmpDir)
+
+		err = os.WriteFile(tmpDir+"/values.txt", []byte("my_var: value"), 777)
+		require.NoError(t, err)
+
+		mockYamlEditor := MockYamlEditor{}
+		err = Walk(tmpDir, &mockYamlEditor)
+		require.NoError(t, err)
+		assert.Empty(t, mockYamlEditor.executedFiles)
+
+	})
+}
+
+func TestFile_Errors(t *testing.T) {
+	t.Run("marshal invalid yaml file into yaml object throws error", func(t *testing.T) {
+		var buffer bytes.Buffer
+		buffer.WriteString("my_val value")
+		content, err := ParseFile(&buffer)
+
+		require.Error(t, err)
+		assert.Empty(t, content)
+	})
+}
